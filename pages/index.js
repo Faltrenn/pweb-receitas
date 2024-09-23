a1a97b3e652932c690e572723b42729567e0b22b
Emanuel Alves <manelzaum@icloud.com>
2024-09-23 14:12:02 -0300

    add: exercises

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import { Casa } from "./teste"
+
 export default function Home(){
     return (
        <div>
           <h2>
             Viva Santana!
+            {Casa()}
           </h2>
        </div>
     )
---
+++ pages/teste.js
@@ -0,0 +1,9 @@
+export function Casa(){
+    return (
+       <div>
+          <h2>
+            Hello, World!
+          </h2>
+       </div>
+    )
+}
