50ca8c73ec163e0aa2d646f00b23843037e854e1
manel <manelzaum@icloud.com>
2025-01-17 19:43:56 -0300

    add: recipe 15.

---
+++ next/rcp15/src/app/login/layout.tsx
@@ -0,0 +1,9 @@
+export default function ContactLayout({ children }: { children: React.ReactNode }) {
+    return (
+      <>
+        <header>[LAYOUT DO CONTACT AQUI]</header>
+        <main>{children}</main>
+      </>
+    );
+  }
+  
